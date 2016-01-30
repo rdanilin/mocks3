5298cade728cbbe10cd42a36532d6b0b8446bc8e
Roman Danilin <rdanilin@aligntech.com>
2016-01-30 09:20:18 -0800

    added support for integration tests

---
+++ src/itest/java/com/github/mocks3/StartupTest.java
@@ -0,0 +1,12 @@
+package com.github.mocks3;
+
+import org.junit.Test;
+
+public class StartupTest {
+
+    @Test
+    public void test() {
+//        Assert.fail("failed");
+        System.out.println("StartupTest.test");
+    }
+}
